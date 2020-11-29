8b422efe7b7590a5f9822fac2cbb723331c718b8
Felix Quast <felix.quast98@gmail.com>
2020-11-29 09:07:12 +0100

    Override createObject in GamingPhotoManager Ensures, that the created Photos are GamingPhotos

--- src/main/java/org/wahlzeit/model/GamingPhotoManager.java
+++ src/main/java/org/wahlzeit/model/GamingPhotoManager.java
@@ -1,5 +1,8 @@
 package org.wahlzeit.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * This class manages the instances of {@link GamingPhoto}.
  */
@@ -7,6 +10,16 @@ public class GamingPhotoManager extends PhotoManager {
 
     public GamingPhotoManager() {
         super();
+    }
+
+    @Override
+    protected Photo createObject(ResultSet rset) throws SQLException {
+        Photo result = super.createObject(rset);
+
+        // when using gaming photo manager, the created photo should be gaming photos respectively
+        assert result instanceof GamingPhoto;
+
+        return result;
     }
 
 }
